426ce4f785f8ecb4ef7e4e5399793d0200d4082c
Logan Wilson <ltwilson4132@gmail.com>
2024-04-06 14:44:55 -0400

    Update Account method is working

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -162,7 +162,7 @@ class TestAccountService(TestCase):
         new_account = resp.get_json()
         new_account["name"] = "John Smith"
         resp = self.client.put(
-            BASE_URL,
+            f"{BASE_URL}/{new_account['id']}",
             json=new_account
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
